b6422ece1a804a4e13ceec8dfe49425d7f16d4b2
Joe Parsley <joewparsley@gmail.com>
2017-08-14 15:40:15 -0700

    add app component to router path

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,15 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+
 
 
 const appRoutes: Routes = [
-
+  {
+     path: '',
+     component: AppComponent
+   },
 
 
 ];
